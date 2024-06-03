733a12dd96889eb53e36bf1f042074074247bfd7
initpa <nitpa.bh@gmail.com>
2024-06-03 19:11:34 +0530

    read operation in crud

--- swift-data/BookListView.swift
+++ swift-data/BookListView.swift
@@ -6,32 +6,65 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct BookListView: View {
+    @Query(sort: \Book.title) private var books: [Book]
     @State private var createNewBook = false
     
     var body: some View {
         NavigationStack {
-            VStack {
-            }
-            .padding()
-            .navigationTitle("MyBooks")
-            .toolbar{
-                Button {
-                    createNewBook = true
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                        .imageScale(.large)
+            Group {
+                if books.isEmpty {
+                    ContentUnavailableView("Please enter a book", systemImage: "book.fill")
+                } else {
+                    List {
+                        ForEach(books) { book in
+                            NavigationLink {
+                                Text(book.title)
+                            } label: {
+                                HStack(spacing: 10) {
+                                    book.icon
+                                    VStack(alignment: .leading) {
+                                        Text(book.title).font(.title)
+                                        Text(book.author).foregroundStyle(.secondary)
+                                        
+                                        if let rating = book.rating {
+                                            HStack {
+                                                ForEach(0..<rating, id: \.self) {_ in
+                                                    Image(systemName: "star.fill")
+                                                        .imageScale(.small)
+                                                        .foregroundStyle(.yellow)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
+                    .padding()
+                    .navigationTitle("MyBooks")
+                    .toolbar{
+                        Button {
+                            createNewBook = true
+                        } label: {
+                            Image(systemName: "plus.circle.fill")
+                                .imageScale(.large)
+                        }
+                    }
+                    .sheet(isPresented: $createNewBook) {
+                        NewBookView()
+                            .presentationDetents([.medium])
+                    }
                 }
-            }
-            .sheet(isPresented: $createNewBook) {
-                NewBookView()
-                    .presentationDetents([.medium])
-            }
+            }                        
         }
     }
 }
 
 #Preview {
     BookListView()
+        .modelContainer(for: Book.self, inMemory: true)
 }
