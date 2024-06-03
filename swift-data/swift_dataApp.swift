b8d5a4b80561ebb22b161efcd030ea87bd1ae2b3
initpa <nitpa.bh@gmail.com>
2024-06-03 18:13:11 +0530

    first commit

---
+++ swift-data/swift_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_dataApp.swift
+//  swift-data
+//
+//  Created by Nitesh Patil on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
