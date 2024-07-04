1ee4fbbc5ccc3eb6310734031d3c73009ced5019
initpa <nitpa.bh@gmail.com>
2024-07-04 22:47:34 +0530

    UIColor and Color Extentions

---
+++ swift-data/Color+Extention.swift
@@ -0,0 +1,21 @@
+//
+//  Color+Extention.swift
+//  swift-data
+//
+//  Created by Nitesh Patil on 04/07/24.
+//
+
+import SwiftUI
+
+extension Color {
+
+    init?(hex: String) {
+        guard let uiColor = UIColor(hex: hex) else { return nil }
+        self.init(uiColor: uiColor)
+    }
+
+    func toHexString(includeAlpha: Bool = false) -> String? {
+        return UIColor(self).toHexString(includeAlpha: includeAlpha)
+    }
+
+}
