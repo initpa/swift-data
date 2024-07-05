7756aa5d1fefff24440fc923787d688c17f1253e
initpa <nitpa.bh@gmail.com>
2024-07-05 22:30:26 +0530

    adding relationships

--- swift-data/Genre.swift
+++ swift-data/Genre.swift
@@ -5,17 +5,21 @@
 //  Created by Nitesh Patil on 04/07/24.
 //
 
-import Foundation
+import SwiftUI
 import SwiftData
-
 
 @Model
 class Genre {
     var name: String
     var color: String
+    var books: [Book]?
     
     init(name: String, color: String) {
         self.name = name
         self.color = color
+    }
+    
+    var hexColor: Color {
+        Color(hex: self.color) ?? .red
     }
 }
